5865fd83a43e1fef03e41e0c6f0a1d08b8cc3e50
Joao P Dubas <joao.dubas@gmail.com>
2019-04-25 16:29:44 +0000

    chore(listings) improve pipedrive task command.
    1. Actually execute update operations when task is called
    2. Rename some methods
    3. Fix some issues show by credo

--- apps/re/lib/mix/tasks/pipedrive/populate.ex
+++ apps/re/lib/mix/tasks/pipedrive/populate.ex
@@ -1,30 +1,37 @@
 defmodule Mix.Tasks.Re.Pipedrive.Populate do
+  @moduledoc """
+  Populate listing details and tags from pipedrive data.
+  """
   use Mix.Task
 
   require Logger
 
   def run(_) do
     Mix.EctoSQL.ensure_started(Re.Repo, [])
+
+    update()
+
+    upsert_tags()
   end
 
   def update() do
-    data()
-    |> data_update()
+    read_data()
+    |> transform_data_for_update()
     |> Enum.map(&operation_update/1)
   end
 
   def upsert_tags() do
-    Enum.map(data(), &operation_upsert/1)
+    Enum.map(read_data(), &operation_upsert/1)
   end
 
-  def data() do
+  def read_data() do
     "priv/deals.json"
     |> File.read!()
     |> Jason.decode!(keys: :atoms)
   end
 
-  def data_update(datum) do
-    datum
+  defp transform_data_for_update(params) do
+    params
     |> Enum.map(fn d -> [:tags, :tags_view] |> Enum.reduce(d, &Map.delete(&2, &1)) end)
     |> Enum.map(fn d ->
       [:orientation, :garage_type, :sun_period] |> Enum.reduce(d, &first_value(&2, &1))
@@ -34,21 +41,22 @@ defmodule Mix.Tasks.Re.Pipedrive.Populate do
     end)
   end
 
-  def operation_update(datum) do
-    with {id, params} <- Map.pop(datum, :id),
+  def operation_update(attrs) do
+    with {id, params} <- Map.pop(attrs, :id),
          {:ok, listing} <- Re.Listings.get(id) do
-      Re.Listing.changeset(listing, params, "admin")
+      listing
+      |> Re.Listing.changeset(params, "admin")
       |> Re.Repo.update()
     end
   end
 
-  def operation_upsert(datum) do
-    with {id, params} <- Map.pop(datum, :id),
+  def operation_upsert(attrs) do
+    with {id, params} <- Map.pop(attrs, :id),
          {:ok, listing} <- Re.Listings.get(id),
          {:ok, tags_uuid} <- fetch_tags(Map.get(params, :tags)) do
       Re.Listings.upsert_tags(listing, tags_uuid)
     else
-      _ -> {:ok, Map.get(datum, :id)}
+      _ -> {:ok, Map.get(attrs, :id)}
     end
   end
 
@@ -99,10 +107,10 @@ defmodule Mix.Tasks.Re.Pipedrive.Populate do
 
   defp transform_value(_, _), do: nil
 
-  def fetch_tags(nil), do: {:error, :no_tags}
-  def fetch_tags([]), do: {:error, :empty_tags}
+  defp fetch_tags(nil), do: {:error, :no_tags}
+  defp fetch_tags([]), do: {:error, :empty_tags}
 
-  def fetch_tags(names_slug) do
+  defp fetch_tags(names_slug) do
     {:ok, Enum.map(Re.Tags.list_by_slugs(names_slug), & &1.uuid)}
   end
 end
